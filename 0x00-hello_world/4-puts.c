ec62b0f81ce066e24d0b539c5ae04b94b075b7ab
Fouad <bourjifouad140@gmail.com>
2023-03-16 06:03:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(voiod)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	retirn (0);
+}
